76f52f036459a546d1a20b913b8764cc20206903
gkuniyil <gkuniyil@ucsd.edu>
2025-08-01 11:49:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+  // scroll to top
+  let button = document.querySelector('.back-to-top');
+  if (button) {
+    button.onclick = function (e) {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+  }
+
+  // like button logic
+  document.querySelectorAll('.likeButton').forEach(button => {
+    button.onclick = function () {
+      const counter = this.querySelector('.likeCount');
+      counter.textContent = Number(counter.textContent) + 1;
+    };
+  });
+
+  
