d9d659396df92ec4013dfc39d24cb0c5e885ab1c
gkuniyil <gkuniyil@ucsd.edu>
2025-08-01 11:11:34 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// scrolling to top when button clicked 
-let buttons = document.querySelector('.back-to-top');
-if (buttons) {
-  buttons.onclick = function(e) {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-}
